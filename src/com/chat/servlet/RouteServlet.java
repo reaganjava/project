4aa8a9d230005cb9ae80cb8618f43a4f4a6f9e61
reading.reagan <reaganjava@gmail.com>
2013-06-18 01:52:48 +0800

    route

--- src/com/chat/servlet/RouteServlet.java
+++ src/com/chat/servlet/RouteServlet.java
@@ -1,6 +1,11 @@
 package com.chat.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,31 +17,40 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/route")
 public class RouteServlet extends HttpServlet {
-	
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RouteServlet() {
-        super();
-    }
 
-    /**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Route");
-		response.setContentType("html/text");
-		response.getWriter().println("Test");
-		response.getWriter().flush();
-		response.getWriter().close();
+	public RouteServlet() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Properties prop = new Properties();
+		InputStream in = getClass().getResourceAsStream(
+				"/config/server.properties");
+		prop.load(in);
+		Set keyValue = prop.keySet();
+		for (Iterator it = keyValue.iterator(); it.hasNext();) {
+			String key = (String) it.next();
+			String value = prop.getProperty(key);
+			System.out.println(key + ":" + value);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 	}
 
 }
